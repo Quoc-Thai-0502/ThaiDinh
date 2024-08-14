0385e89b27199ef881f8ed1b4bbbcaea2f630623
thaidinh <thaidinh23091@gmail.com>
2024-08-14 20:06:39 +0700

    lhahaha

--- js/index.js
+++ js/index.js
@@ -33,24 +33,117 @@ function typeWriter() {
 // Đảm bảo hàm typeWriter được gọi khi cửa sổ tải
 window.onload = typeWriter;
 
+
+
 // Particles.js configuration
-particlesJS('particles-js', {
-    particles: {
-        number: { value: 100, density: { enable: true, value_area: 800 } },
-        color: { value: "#ffffff" },
-        shape: { type: "circle", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 10 } },
-        opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.4, sync: false } },
-        size: { value: 9, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-        line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
-        move: { enable: true, speed: 6, direction: "none", random: false, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1500 } }
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": false,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#f1f1f1" // Màu trắng sáng cho hạt
+      },
+      "shape": {
+        "type": "star", // Kiểu hình dạng là ngôi sao
+        "stroke": {
+          "width": 0,
+          "color": "#ffffff"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 10, // Tăng độ mờ lên 1 để làm cho hạt rõ ràng nhất có thể
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0, // Độ mờ tối thiểu là 1
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 14, // Tăng kích thước của hạt
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 100,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 9, // Giảm tốc độ di chuyển để hạt dễ thấy hơn
+        "direction": "left",
+        "random": false,
+        "straight": true,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
     },
-    interactivity: {
-        detect_on: "canvas",
-        events: { onhover: { enable: true, mode: "repulse" }, onclick: { enable: true, mode: "push" }, resize: true },
-        modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 6 }, repulse: { distance: 200, duration: 0.4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } }
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
     },
-    retina_detect: true
-});
-
-
-
+    "retina_detect": true
+  });
+  
